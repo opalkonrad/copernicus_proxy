70a1cfad63a449a509046684bf436e9b87f85111
wfabjanc <wojciechfabjanczuk@gmail.com>
2019-04-22 13:55:27 +0200

    downloader/sea_level path slash fix

--- downloader/urls.py
+++ downloader/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sea_level', views.SeaLevelView.as_view(), name='sea_level')
+    path('sea_level/', views.SeaLevelView.as_view(), name='sea_level')
 ]
