84012a6986833ab9e4105b78a86de28d76474c42
Wiktor <wiktor.michalski.2@gmail.com>
2019-04-22 17:41:47 +0200

    some lil testing

--- downloader/forms/sea_level_form.py
+++ downloader/forms/sea_level_form.py
@@ -3,8 +3,8 @@ from django import forms
 
 class SeaLevelForm(forms.Form):
     year_choice = (
-        ("1", "1"),
-        ("2", "2"),
-        ("3", "3"),
+        ("1998", "year 1998"),
+        ("1999", "year 1999"),
+        ("2000", "year 2000"),
         )
     years = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=year_choice)
