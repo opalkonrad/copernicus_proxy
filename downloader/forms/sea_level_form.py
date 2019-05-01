30388b41f135f14f3b9083b4abf48087e659bc9b
wfabjanc <wojciechfabjanczuk@gmail.com>
2019-05-01 22:12:23 +0200

    SeaLevelForm errors display

--- downloader/forms/sea_level_form.py
+++ downloader/forms/sea_level_form.py
@@ -3,9 +3,9 @@ import downloader.forms.sea_level_choices as options
 
 
 class SeaLevelForm(forms.Form):
-    years = forms.CharField(required=True)
-    months = forms.CharField(required=True)
-    days = forms.CharField(required=True)
+    years = forms.CharField(required=True, min_length=3)
+    months = forms.CharField(required=True, min_length=3)
+    days = forms.CharField(required=True, min_length=3)
     format = forms.ChoiceField(
         widget=forms.Select,
         choices=options.formats,
