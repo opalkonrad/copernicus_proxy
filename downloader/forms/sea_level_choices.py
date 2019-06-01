ac8c961455b88d700fb48179ea51289c848bac9c
wfabjanczuk <wojciechfabjanczuk@gmail.com>
2019-06-01 12:31:02 +0200

    Product types and hours added to form

--- downloader/forms/sea_level_choices.py
+++ downloader/forms/sea_level_choices.py
@@ -1,8 +1,9 @@
-from downloader.constants import formats, days, months, years
+from downloader.constants import formats, hours, days, months, years, product_types
 
 years_list = []
 months_list = []
 days_list = []
+hours_list = []
 formats_list = []
 
 for i in range(years.MIN, years.MAX + 1):
@@ -14,10 +15,15 @@ for i in range(months.MIN, months.MAX + 1):
 for i in range(days.MIN, days.MAX + 1):
     days_list.append((i, i))
 
+for i in range(hours.MIN, hours.MAX + 1):
+    hours_list.append((i, "{:02d}".format(i) + ":00"))
+
 for f in formats.list:
     formats_list.append((f.extension[0], f.name))
 
+product_types = tuple(product_types.product_types_list)
 years = tuple(years_list)
 months = tuple(months_list)
 days = tuple(days_list)
+hours = tuple(hours_list)
 formats = tuple(formats_list)
