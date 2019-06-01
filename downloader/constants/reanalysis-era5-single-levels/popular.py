fe83652d99e230c34cc83d4f75e0637ab4f10f3a
wfabjanczuk <wojciechfabjanczuk@gmail.com>
2019-06-01 12:57:37 +0200

    Popular filters

---
+++ downloader/constants/reanalysis-era5-single-levels/popular.py
@@ -0,0 +1,46 @@
+filters = [
+    [
+        "10m_u_component_of_wind",
+        "10m u component of wind"
+    ],
+    [
+        "10m_v_component_of_wind",
+        "10m v component of wind"
+    ],
+    [
+        "2m_dewpoint_temperature",
+        "2m dewpoint temperature"
+    ],
+    [
+        "2m_temperature",
+        "2m temperature"
+    ],
+    [
+        "mean_sea_level_pressure",
+        "Mean sea level pressure"
+    ],
+    [
+        "mean_wave_direction",
+        "Mean wave direction"
+    ],
+    [
+        "mean_wave_period",
+        "Mean wave period"
+    ],
+    [
+        "sea_surface_temperature",
+        "Sea surface temperature"
+    ],
+    [
+        "significant_height_of_combined_wind_waves_and_swell",
+        "Significant height of combined wind waves and swell"
+    ],
+    [
+        "surface_pressure",
+        "Surface pressure"
+    ],
+    [
+        "total_precipitation",
+        "Total precipitation"
+    ]
+]
