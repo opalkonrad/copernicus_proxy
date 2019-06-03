8f1c6d0e281ecd88d899754c4c3f1281b797cb3f
wfabjanczuk <wojciechfabjanczuk@gmail.com>
2019-06-03 14:12:53 +0200

    Fixed form submit

--- downloader/forms/sea_level_form.py
+++ downloader/forms/sea_level_form.py
@@ -3,12 +3,12 @@ import downloader.forms.sea_level_choices as options
 
 
 class SeaLevelForm(forms.Form):
-    product_types = forms.CharField(required=True, min_length=3)
-    popular_filters = forms.CharField(required=True, min_length=3)
+    # product_types = forms.CharField(required=True, min_length=3)
+    # popular_filters = forms.CharField(required=True, min_length=3)
     years = forms.CharField(required=True, min_length=3)
     months = forms.CharField(required=True, min_length=3)
     days = forms.CharField(required=True, min_length=3)
-    hours = forms.CharField(required=True, min_length=3)
+    # hours = forms.CharField(required=True, min_length=3)
     format = forms.ChoiceField(
         widget=forms.Select,
         choices=options.formats,
