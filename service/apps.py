b4e2f3b5e1ca606ef2d31e86f82eda1d29bc2a23
wfabjanczuk <wojciechfabjanczuk@gmail.com>
2019-06-06 02:51:02 +0200

    Models added to service

--- service/apps.py
+++ service/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+import os
 
 
 class ServiceConfig(AppConfig):
     name = 'service'
+
+
+DOWNLOADER_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATES_DIR = os.path.join(DOWNLOADER_DIR, 'templates')
--- service/models.py
+++ service/models.py
@@ -1,26 +1,35 @@
 from django.db import models
 from django.utils import timezone
+import jsonfield
 
-CONTENT_MAX_LENGTH = 512
 STATUS_MAX_LENGTH = 16
 MSG_MAX_LENGTH = 512
-DATA_SET_NAME_MAX_LENGTH = 128
+DATA_SET_MAX_LENGTH = 128
 ATTRIBUTES_MAX_LENGTH = 256
 
 
 class Task(models.Model):
     task_date = models.DateTimeField(default=timezone.now)
-    content = models.CharField(max_length=CONTENT_MAX_LENGTH)
+    json_content = jsonfield.JSONField()
     status = models.CharField(max_length=STATUS_MAX_LENGTH, default="pending")
-    data_set = models.TextField(max_length=DATA_SET_NAME_MAX_LENGTH, default="???")
+    data_set = models.TextField(max_length=DATA_SET_MAX_LENGTH, default="???")
     msg = models.CharField(max_length=MSG_MAX_LENGTH)
 
 
-# DataSets Foreign Key TBA
-# DownloadedFile Foreign Key TBA
-
 class DataSets(models.Model):
-    data_set_name = models.CharField(max_length=DATA_SET_NAME_MAX_LENGTH)
+    data_set = models.CharField(max_length=DATA_SET_MAX_LENGTH, unique=True)
     attributes = models.CharField(max_length=ATTRIBUTES_MAX_LENGTH)
 
+# DataSets Foreign Key TBA
+# DownloadedFile Foreign Key TBA
 # DownloadedFile TBA
+
+
+# class DownloadedFile(models.Model):
+#     date = models.DateTimeField(default=timezone.now)
+#     path = models.CharField(max_length=255)
+#     size = models.CharField(max_length=31)
+#     fk = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+#     def set_path(self, category):
+#         self.path = "./files/" + category + "/file_id_" + self.fk
