12b93e29ce41cb559cc7456031911e3f9248b483
wfabjanczuk <wojciechfabjanczuk@gmail.com>
2019-06-14 22:10:44 +0000

    Installation script for Ubuntu 18.04

---
+++ install_ubuntu_18-04.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.7
+
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt install python3-pip
+
+sudo apt-get install libpq-dev libpython3.7-dev rabbitmq-server python-celery-common
+
+python3.7 -m pip install -r requirements.txt
